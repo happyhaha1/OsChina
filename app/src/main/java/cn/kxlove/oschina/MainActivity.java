21eeac84762fec6586db966f08fec7b366b777fa
童lian话 <474248257@qq.com>
2016-05-25 14:39:21 +0800

    初始化程序

---
+++ app/src/main/java/cn/kxlove/oschina/MainActivity.java
@@ -0,0 +1,13 @@
+package cn.kxlove.oschina;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
